f7d0b467282ab7522c758397731321f4f2cb24f9
carpek <77640624+carpek@users.noreply.github.com>
2021-04-26 20:07:19 +0200

    added the git link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+
+<p>
+  This project was made by Lina Pekar: 
+     <a href="https://github.com/carpek/react-weather-app-new" target="_blank"></a>
     </div>
+    </p>
   );
 }
 
